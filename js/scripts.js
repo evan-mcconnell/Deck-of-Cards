509b0e36795409c0d690768a52b10c26efdbafd0
kimmcconnell <kmcconnell393@gmail.com>
2019-02-04 11:46:08 -0800

    separate some of the business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
 //buisness logic
 var suits = ["hearts", "spades", "clubs", "diamonds"]
 var sets = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
-var deck = 0;
+var deck = [];
 
+var makeDeck = function() {
+  sets.forEach(function(set) {
+    suits.forEach(function(suit) {
+      deck.push("<li>" + set + " of " + suit + "</li>");
+    });
+  });
+};
 
 //user logic
 $(document).ready( function() {
   $("#deck-button").click( function(event) {
   event.preventDefault();
-  deck = sets.forEach(function(set) {
-    suits.forEach(function(suit) {
-      $("ul#deck-list").append("<li>" + set + " of " + suit + "</li>");
+    makeDeck();
+    deck.forEach(function(card) {
+    $("ul#deck-list").append(card);
     });
-  });
-
-
-  console.log(deck);
   });
 });
