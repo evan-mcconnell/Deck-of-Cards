82c4f98617bcbd857a7260a56be6fe5c0c9efa8c
kimmcconnell <kmcconnell393@gmail.com>
2019-02-04 12:17:54 -0800

    add semi-functional random card generatorthat mistakenly contains list syntax currently

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 var suits = ["hearts", "spades", "clubs", "diamonds"]
 var sets = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
 var deck = [];
+var randomCard = 0;
 
 var makeDeck = function() {
   sets.forEach(function(set) {
@@ -11,13 +12,22 @@ var makeDeck = function() {
   });
 };
 
+var randomCardMaker = function() {
+  return deck[Math.floor(Math.random()*deck.length)];
+};
+
 //user logic
 $(document).ready( function() {
   $("#deck-button").click( function(event) {
   event.preventDefault();
     makeDeck();
     deck.forEach(function(card) {
-    $("ul#deck-list").append(card);
+      $("ul#deck-list").append(card);
     });
+  });
+
+  $("#random-card-btn").click( function() {
+    randomCard = randomCardMaker();
+    console.log(randomCard);
   });
 });
